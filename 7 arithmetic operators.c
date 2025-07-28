29c375f2a68454b54a4aa21f8e200547159e08cd
Shreya Doshi <37837168+shreyabdoshi@users.noreply.github.com>
2025-07-28 22:35:57 +0530

    Add files via upload

---
+++ 7 arithmetic operators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	printf("Enter the value of a");
+	scanf("%f",&a);
+	printf("Enter the value of b");
+	scanf("%f",&b);
+	printf("Addition is %f",a+b);
+	printf("Subtraction is %f",a-b);
+	printf("Multiplication is %f",a*b);
+	printf("Division is %f",a/b);
+	return 0;
+	
+}
